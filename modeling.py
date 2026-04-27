9f73dece2586988045186fc816126aec36c9c7d8
Thiago Hideki <thiago.tsutiya@gmail.com>
2026-04-27 11:17:25 -0300

    model training

--- modeling.py
+++ modeling.py
@@ -1,44 +1,141 @@
-from statistics import LinearRegression
-
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.metrics import mean_squared_error, r2_score
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import loguniform, randint
+from sklearn.model_selection import LeaveOneOut, RandomizedSearchCV, GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+# from sklearn.metrics import mean_squared_error, r2_score
 
 df = pd.read_csv("clean_editable_plants.csv")
 
 # Guardamos os nomes em uma variável separada para referência futura
-nomes_plantas = df['common_name']
+nomes_plantas = df["common_name"]
 
-# Definimos X excluindo as colunas de identificação e as que já transformamos em valores numéricos, e o alvo (y)
-X = df.drop(columns=['water_code', 'taxonomic_name', 'common_name', 'water', 'nutrients', 'temperature_class'])
-y = df['water_code']
-
-# Guardamos os índices durante a divisão de treino e teste
-X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(
-    X, y, df.index, test_size=0.2, random_state=42
+df = df.drop(
+    columns=[
+        "taxonomic_name",
+        "common_name",
+        "water",
+        "nutrients",
+        "temperature_class",
+    ]
 )
 
-models = {
-    "Linear Regression": LinearRegression(),
-    "Random Forest": RandomForestRegressor(n_estimators=100, random_state=42),
-    "KNN": KNeighborsRegressor(n_neighbors=5)
+
+# Definimos X excluindo as colunas de identificação e as que já transformamos em valores numéricos, e o alvo (y)
+y = df["water_code"]
+X = df.drop(
+    columns=[
+        "water_code",
+    ]
+)
+
+loo = LeaveOneOut()
+
+# LOGISTIC REGRESSION
+# Criamos uma pipeline para padronizar os dados antes de rodar o LOO.
+log_pipe = Pipeline(
+    [
+        ("scaper", StandardScaler()),
+        ("log_reg", LogisticRegression(max_iter=1000, random_state=0)),
+    ]
+)
+
+log_param_dist = {"log_reg__C": loguniform(1e-3, 1e3)}
+
+random_search = RandomizedSearchCV(
+    estimator=log_pipe,
+    param_distributions=log_param_dist,
+    n_iter=100,
+    cv=loo,
+    scoring="accuracy",
+    n_jobs=-1,
+    random_state=0,
+)
+
+random_search.fit(X, y)
+
+c_values = random_search.cv_results_["param_log_reg__C"].data.astype(float)
+scores = random_search.cv_results_["mean_test_score"]
+
+plt.figure(figsize=(8, 5))
+plt.scatter(c_values, scores)
+plt.xscale("log")
+plt.xlabel("Parâmetro C")
+plt.ylabel("Acurácia Média (LOOCV)")
+plt.title("Regressão Logística: Ajuste de parâmetro")
+plt.grid(True, which="both", alpha=0.5)
+plt.show()
+
+print(
+    f"Melhor parâmetro (C = 1/lambda): {random_search.best_params_['log_reg__C']:.4f}"
+)
+print(f"Lambda: {1 / random_search.best_params_['log_reg__C']:.4f}")
+print(f"Melhor acurácia LOOCV: {random_search.best_score_:.4f}")
+
+# KNN REGRESSION
+knn_pipe = Pipeline(
+    [
+        ("scaper", StandardScaler()),
+        ("knn", KNeighborsClassifier()),
+    ]
+)
+
+knn_param_grid = {"knn__n_neighbors": np.arange(1, 31)}
+
+knn_grid = GridSearchCV(
+    estimator=knn_pipe, param_grid=knn_param_grid, cv=loo, scoring="accuracy", n_jobs=-1
+)
+
+knn_grid.fit(X, y)
+
+k_values = knn_param_grid["knn__n_neighbors"]
+knn_scores = knn_grid.cv_results_["mean_test_score"]
+
+plt.figure(figsize=(8, 5))
+plt.plot(k_values, knn_scores)
+plt.xlabel("Número de vizinhos(K)")
+plt.ylabel("Acurácia Média (LOOCV)")
+plt.title("KNN: Ajuste de parâmetro")
+plt.grid(True, which="both")
+plt.show()
+
+print(f"Melhor K: {knn_grid.best_params_['knn__n_neighbors']}")
+print(f"Melhor acurácia KNN: {knn_grid.best_score_:.4f}\n")
+
+# Random forest
+rf = RandomForestClassifier(n_estimators=100, random_state=0)
+
+rf_param_dist = {
+    "max_depth": randint(2, 30),
 }
 
-# Loop de treino e avaliação
-# MSE = Erro quadrático médio
-# R2 Score = Coeficiente de determinação, que indica a proporção da variância dos dados que é explicada pelo modelo. Quanto mais próximo de 1 (100%), melhor o modelo se encaixa aos dados.
-print(f"{'Modelo':<20} | {'MSE':<10} | {'R2 Score':<10}")
-print("-" * 40)
+rf_random = RandomizedSearchCV(
+    estimator=rf,
+    param_distributions=rf_param_dist,
+    n_iter=15,
+    cv=loo,
+    scoring="accuracy",
+    n_jobs=-1,
+    random_state=0,
+)
 
-for nome, modelo in models.items():
-    modelo.fit(X_train, y_train)
-    pred = modelo.predict(X_test)
+rf_random.fit(X, y)
 
-    # Reccebem o y_test e as predições do modelo para calcular as métricas de avaliação
-    mse = mean_squared_error(y_test, pred)
-    r2 = r2_score(y_test, pred)
-    
-    print(f"{nome:<20} | {mse:<10.4f} | {r2:<10.4f}")
+depth_vals = rf_random.cv_results_["param_max_depth"].data.astype(int)
+rf_scores = rf_random.cv_results_["mean_test_score"]
+
+plt.figure(figsize=(8, 5))
+plt.scatter(depth_vals, rf_scores)
+plt.xlabel("Máxima Profundidade")
+plt.ylabel("Acurácia Média (LOOCV)")
+plt.title("Random Forest: Ajuste de parâmetro")
+plt.grid(True)
+plt.show()
+
+print(f"Melhor Profundidade: {rf_random.best_params_['max_depth']}")
+print(f"Melhor acurácia RF: {rf_random.best_score_:.4f}")
